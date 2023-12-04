cc42ee0969570587566217d12472ee63d3225798
kuuyyaa <146645814+kuuyyaa@users.noreply.github.com>
2023-12-04 23:54:49 +0000

    Event listeners for quiz start and score save

--- assets/logic.js
+++ assets/logic.js
@@ -8,3 +8,25 @@ var finalScoreDisplay = document.getElementById("final-score");
 var initialsInput = document.getElementById("initials");
 var submitButton = document.getElementById("submit");
 
+// Additional Variables needed for time variables
+var secondsLeft;
+var score;
+var currentQuestionIndex;
+var timer;
+
+
+
+// Event listener for the start button
+startButton.addEventListener("click", startQuiz);
+
+// Event listener for the submit button
+submitButton.addEventListener("click", saveScore);
+
+
+function startQuiz() {
+
+} 
+
+function saveScore() {
+  
+}
