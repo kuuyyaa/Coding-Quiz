bfae98206f08adbc65874d853094138de1edcaa4
kuuyyaa <146645814+kuuyyaa@users.noreply.github.com>
2023-12-04 23:50:56 +0000

    HTML element references

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,10 @@
+// References for HTML elements
+var startButton = document.getElementById("start");
+var timerDisplay = document.getElementById("time");
+var questionTitle = document.getElementById("question-title");
+var choicesContainer = document.getElementById("choices");
+var endScreen = document.getElementById("end-screen");
+var finalScoreDisplay = document.getElementById("final-score");
+var initialsInput = document.getElementById("initials");
+var submitButton = document.getElementById("submit");
 
--- assets/questions.js
+++ assets/questions.js
@@ -5,7 +5,36 @@ const questions = [
         answers: ["A programming language", "A fruit", "A car brand", "A type of coffee"],
         correctAnswer: "A programming language"
     },
-    // Add more questions as needed
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "3"
+    },
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "4"
+    },
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "4"
+    },
+    {
+        question: "Question?",
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "3"
+    },
 ];
 
 // Other variables and functions needed for your quiz logic
@@ -15,7 +44,7 @@ document.getElementById("start-button").addEventListener("click", startQuiz);
 
 // Function to start the quiz
 function startQuiz() {
-    // Implement your quiz logic here
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
 }
 
 // Add more functions as needed
