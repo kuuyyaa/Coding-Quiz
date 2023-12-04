5358cf91ca2f005e7b65959fb51ca0c63e131860
kuuyyaa <146645814+kuuyyaa@users.noreply.github.com>
2023-12-04 23:11:24 +0000

    initial starter commit

---
+++ assets/questions.js
@@ -0,0 +1,21 @@
+// Define your questions and answers array
+const questions = [
+    {
+        question: "What is JavaScript?",
+        answers: ["A programming language", "A fruit", "A car brand", "A type of coffee"],
+        correctAnswer: "A programming language"
+    },
+    // Add more questions as needed
+];
+
+// Other variables and functions needed for your quiz logic
+
+// Event listener for the start button
+document.getElementById("start-button").addEventListener("click", startQuiz);
+
+// Function to start the quiz
+function startQuiz() {
+    // Implement your quiz logic here
+}
+
+// Add more functions as needed
